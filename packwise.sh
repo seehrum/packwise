ae7da2debb4b5bcbbb21708eb635939e6456209d
seehrum <raphaelciribelly@gmail.com>
2024-05-08 08:34:53 -0300

    packwise.sh

--- packwise.sh
+++ packwise.sh
@@ -2,6 +2,9 @@
 # PackWise v1.0
 # GNU bash, versão 5.1.16(1)-release (x86_64-pc-linux-gnu)
 
+# useful commands
+# sed -i 's/\[ADD\]\|\[REMOVE\]/[\*]/g' packages.list # makes all packages from packages.list go to the removal area
+
 # Define the file that contains the package list
 PACKAGE_FILE="packages.list"
 INSTALL_LOG="addpkg.log"
@@ -9,17 +12,35 @@ REMOVE_LOG="removepkg.log"
 
 # Function to show help instructions
 show_help() {
+    echo "PackWise v1.0 - Advanced Package Management Utility"
     echo "Usage: $0 [option] [argument]"
+    echo
     echo "Options:"
-    echo "  -i                             Install packages listed as [ADD] in $PACKAGE_FILE"
-    echo "  -r                             Remove packages listed as [REMOVE] in $PACKAGE_FILE"
-    echo "  -A package-name                Add a package to the $PACKAGE_FILE with [ADD] prefix"
-    echo "  -R package-name                Replace [ADD] with [REMOVE] in $PACKAGE_FILE for the specified package"
-    echo "  -l                             Generate a list of all installed packages with [ADD] prefix"
-    echo "  -f filename                    Add or modify packages from a provided list file"
-    echo "  -c                             checks for duplicate package names in packages.list"
-    echo "  -h                             Display this help"
+    echo "  -i                             Install all packages marked with [ADD] in $PACKAGE_FILE."
+    echo "  -r                             Remove all packages marked with [REMOVE] from the system and clean entries from $PACKAGE_FILE."
+    echo "  -A package-name                Add a new package entry with [ADD] prefix to $PACKAGE_FILE. Marks package for installation."
+    echo "  -R package-name                Completely remove any package entry (regardless of its current state) from $PACKAGE_FILE."
+    echo "  -S package-name                Toggle the status of a specific package between [ADD], [REMOVE], and [*]."
+    echo "  -l                             Re-generate $PACKAGE_FILE with all currently installed packages, marked with [ADD]."
+    echo "  -f filename                    Import package names from a specified file and add them to $PACKAGE_FILE with [ADD] prefix, ready for installation."
+    echo "  -c                             Check for and report any duplicate package entries in $PACKAGE_FILE, helping maintain a clean package list."
+    echo "  -T                             Toggle all packages in $PACKAGE_FILE between [ADD] and [*], facilitating bulk changes in package statuses."
+    echo "  -h                             Display this help message and exit."
+    echo
+    echo "Understanding packages.list:"
+    echo "  - [ADD]: Packages marked with [ADD] are scheduled for installation."
+    echo "  - [REMOVE]: Packages marked with [REMOVE] are scheduled for uninstallation and will be removed from the system."
+    echo "  - [*]: Packages marked with [*] are ignored during installation or removal processes."
+    echo
+    echo "Logging:"
+    echo "  - All installation actions are logged to $INSTALL_LOG for auditing and review."
+    echo "  - All removal actions are logged to $REMOVE_LOG for tracking and verification."
+    echo
+    echo "Note:"
+    echo "  - Ensure you have the necessary administrative permissions to perform installation and removal of packages."
+    echo "  - Operations such as -i and -r require sudo privileges to modify system packages."
 }
+
 
 # Function to check if packlist.list exists
 check_packlist_exist() {
@@ -61,8 +82,8 @@ remove_packages() {
 }
 
 # Function to add a package to the list
-add_package() {
-    if grep -qE "\[ADD\]$1|\[REMOVE\]$1" $PACKAGE_FILE; then
+add_package_list() {
+    if grep -qE "\[\*\]$1|\[ADD\]$1|\[REMOVE\]$1" $PACKAGE_FILE; then
         echo "Package $1 is already in the list."
     else
         echo "[ADD]$1" >> $PACKAGE_FILE
@@ -70,28 +91,36 @@ add_package() {
     fi
 }
 
-# Function to replace [ADD] with [REMOVE] in the list
-replace_with_remove() {
-    if grep -q "\[ADD\]$1" $PACKAGE_FILE; then
-        sed -i "s/\[ADD\]$1/\[REMOVE\]$1/" $PACKAGE_FILE
-	echo "package added to $PACKAGE_FILE for removal"
+# Function to remove a package from the list completely
+remove_package_list() {
+    # Verificar se o pacote com prefixo existe no arquivo
+    if grep -qE "^\[(ADD|REMOVE|\*)\]$1\$" "$PACKAGE_FILE"; then
+        # Usar sed para remover a linha seguinte ao pacote com o prefixo
+        sed -i -r "/^\[(ADD|REMOVE|\*)\]$1\$/ {N; d;}" "$PACKAGE_FILE"
+        echo "Package $1 removed from $PACKAGE_FILE."
     else
-        echo "Package $1 not found as [ADD]"
+        echo "Package $1 not found in the list."
     fi
 }
 
-# Function to handle adding from a provided list
-#add_from_list() {
-#    while read -r line; do
-#        if grep -qE "\[ADD\]$line|\[REMOVE\]$line" $PACKAGE_FILE; then
-#            sed -i "s/\[REMOVE\]$line/\[ADD\]$line/" $PACKAGE_FILE
-#	   echo "Packages added, some packages already exist and have been added to $PACKAGE_FILE for non-removal area"
-#        else
-#            echo "[ADD]$line" >> $PACKAGE_FILE
-#	   echo "packages added to $PACKAGE_FILE for installation"
-#        fi
-#    done < "$1"
-#}
+# Function to toggle package status between [ADD], [REMOVE], and [*]
+toggle_package_status() {
+    if grep -q "\[ADD\]$1" $PACKAGE_FILE; then
+        # Change from [ADD] to [REMOVE]
+        sed -i "s/\[ADD\]$1/\[REMOVE\]$1/" $PACKAGE_FILE
+        echo "Package $1 status changed to [REMOVE] in $PACKAGE_FILE."
+    elif grep -q "\[REMOVE\]$1" $PACKAGE_FILE; then
+        # Change from [REMOVE] to [ADD]
+        sed -i "s/\[REMOVE\]$1/\[ADD\]$1/" $PACKAGE_FILE
+        echo "Package $1 status changed to [ADD] in $PACKAGE_FILE."
+    elif grep -q "\[\*\]$1" $PACKAGE_FILE; then
+        # Change from [*] to [ADD]
+        sed -i "s/\[\*\]$1/\[ADD\]$1/" $PACKAGE_FILE
+        echo "Package $1 status changed to [ADD] in $PACKAGE_FILE."
+    else
+        echo "Package $1 not found in any status in $PACKAGE_FILE."
+    fi
+}
 
 # Function to handle adding from a provided list
 add_from_list() {
@@ -129,10 +158,27 @@ check_repeated_packages() {
     fi
 }
 
+# Function to toggle package statuses between [ADD], [REMOVE], and [*]
+toggle_packages() {
+    # Check if there are any [ADD] or [REMOVE] entries and toggle to [*]
+    if grep -qE "\[ADD\]|\[REMOVE\]" $PACKAGE_FILE; then
+        echo "Toggling [ADD] and [REMOVE] to [*]"
+        sed -i 's/\(\[ADD\]\|\[REMOVE\]\)/[*]/g' $PACKAGE_FILE
+    # If no [ADD] or [REMOVE] found, check for [*] and toggle to [ADD]
+    elif grep -qE "^\[\*\]" $PACKAGE_FILE; then
+        echo "Toggling [*] to [ADD]"
+        sed -i 's/^\[\*\]/[ADD]/g' $PACKAGE_FILE
+    # Else toggle [ADD] to [REMOVE]
+    else
+        echo "Toggling [ADD] to [REMOVE]"
+        sed -i 's/\[ADD\]/[REMOVE]/g' $PACKAGE_FILE
+    fi
+}
+
 OPT_FOUND=0
 
 # Parse command line options
-while getopts ":iraA:R:lcf:h" opt; do
+while getopts ":iraA:R:lcf:hTS" opt; do
     case ${opt} in
         i ) install_packages
             OPT_FOUND=1
@@ -140,12 +186,15 @@ while getopts ":iraA:R:lcf:h" opt; do
         r ) remove_packages
             OPT_FOUND=1
             ;;
-        A ) add_package "$OPTARG"
+        A ) add_package_list "$OPTARG"
             OPT_FOUND=1
             ;;
-        R ) replace_with_remove "$OPTARG"
+        R ) remove_package_list "$OPTARG"
             OPT_FOUND=1
             ;;
+        S ) toggle_package_status "$OPTARG"
+            OPT_FOUND=1
+	   ;;
         l ) create_package_list
             OPT_FOUND=1
             ;;
@@ -153,6 +202,9 @@ while getopts ":iraA:R:lcf:h" opt; do
             OPT_FOUND=1
             ;;
         c ) check_repeated_packages 
+            OPT_FOUND=1
+	   ;;
+        T ) toggle_packages
             OPT_FOUND=1
 	   ;;
         h ) show_help
